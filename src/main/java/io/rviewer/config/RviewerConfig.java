f0a51ef3d59fa2d2c7322fe00ce0fc30d3adf0e3
ANIMASHAUN HABEEB <animashaunhabeeb@ANIMASHAUNs-MacBook-Pro.local>
2023-07-18 11:43:49 +0100

    Setting Up Remote Project Branch

--- src/main/java/io/rviewer/config/RviewerConfig.java
+++ src/main/java/io/rviewer/config/RviewerConfig.java
@@ -3,7 +3,11 @@ package io.rviewer.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Configuration
 public class RviewerConfig {
@@ -11,7 +15,17 @@ public class RviewerConfig {
     @Bean
     public RestTemplate restTemplate()
     {
-        RestTemplate restTemplate = new  RestTemplateBuilder().build();
+        RestTemplate restTemplate = new  RestTemplateBuilder().errorHandler(defaultResponseErrorHandler()). build();
         return restTemplate;
+    }
+
+    private DefaultResponseErrorHandler defaultResponseErrorHandler()
+    {
+        return new DefaultResponseErrorHandler(){
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                super.handleError(response);
+            }
+        };
     }
 }
